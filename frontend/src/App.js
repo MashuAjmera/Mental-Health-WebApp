43c7a6da4a42e48f55ebb24614198222df272c1a
Danshu Jain <mashuajmera@gmail.com>
2020-03-31 22:41:33 +0530

    frontend react app created

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import { BrowserRouter as Router, Route} from "react-router-dom";
+import './App.css';
+
+// import Navbar from './components/navbar.component'
+// import Us from './components/us.component'
+// import Express from './components/express.component'
+// import Home from './components/home.component'
+// import Albums from './components/albums.component'
+// import Me from './components/me.component'
+
+function App() {
+  return (
+    <div>Hey!</div>
+  //   <Router>
+  //   <div className="container">
+  //     <Route path="/us" component={Us} />
+  //     <Route path="/express" component={Express} />
+  //     <Route path="/" exact component={Home} />
+  //     <Route path="/albums" component={Albums} />
+  //     <Route path="/me" component={Me} />
+  //     <br/>
+  //     <Navbar />
+  //   </div>
+  // </Router>
+  );
+}
+
+export default App;
