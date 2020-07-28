ac4295f25c27f23162573b22af7160ed61299dc2
Mashu Ajmera <mashuajmera@gmail.com>
2020-07-28 21:51:32 +0530

    setup

--- server.js
+++ server.js
@@ -1,50 +1,53 @@
-const express=require('express');
-const mongoose=require('mongoose');
-require('dotenv').configure();
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
 
-const app=express();
+const port = process.env.PORT;
+const uri = process.env.URI;
 
-const port=process.env.PORT||5000;
-const uri=process.env.URI;
-const albumsRouter = require('./routes/albums');
+// const albumsRouter = require("./routes/albums.route");
+// const dataRouter = require("./routes/auth.route");
+// const listRouter = require("./routes/list.route");
+// const storiesRouter = require("./routes/stories.route");
+// const tracksRouter = require("./routes/tracks.route");
+// const usersRouter = require("./routes/users.route");
 
-const dataRouter = require('./routes/data');
-
-
-const listRouter = require('./routes/list');
-
-const storiesRouter = require('./routes/stories');
-
-
-const tracksRouter = require('./routes/tracks');
-
-const usersRouter = require('./routes/users');
-
-
-
+const app = express();
 app.use(express.json());
-app.use('/albums', albumsRouter );
+app.use(cors()); // CORS Middleware
 
-app.use('/data', dataRouter );
+// app.use("/albums", albumsRouter);
+// app.use("/data", dataRouter);
+// app.use("/list", listRouter);
+// app.use("/stories", storiesRouter);
+// app.use("/tracks", tracksRouter);
+// app.use("/users", usersRouter);
 
-app.use('/list', listRouter );
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("views/build"));
 
-app.use('/stories', storiesRouter );
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views", "build", "index.html"));
+  });
+}
 
-app.use('/tracks', tracksRouter );
+// mongoose
+//   .connect(uri, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     const connection = mongoose.connection;
+//     connection.once("open", () => {
+//       console.log("MongoDB database connection established successfully.");
+//     });
+//   })
+//   .catch((error) => console.error(error));
 
-app.use('/users', usersRouter );
-
-
-
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
-
-
-
-app.listen(port,console.log('Server is running on port: ${port}'));
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
